16a1fff3cba1d1938bda2909030eb8b4dde1c6e1
Dominic52 <dongyuanyang@hotmail.com>
2018-09-26 20:14:09 +1200

    added iteration 4 base

---
+++ iteration 4/maps.py
@@ -0,0 +1,44 @@
+class Map(object):
+    """ Stores details on a map. """
+
+    def __init__(self, name, filepath):
+        self.name = name
+        self.filepath = filepath
+
+
+class MapStore(object):
+    """ MapStore stores all the maps for DALSys. """
+
+    def __init__(self, conn=None):
+        self._maps = {}
+        self._conn = conn
+
+    def add(self, map):
+        """ Adds a new map to the store. """
+        if map.name in self._maps:
+            raise Exception('Map already exists in store')
+        else:
+            self._maps[map.name] = map
+
+    def remove(self, map):
+        """ Removes a map from the store. """
+        if not map.name in self._maps:
+            raise Exception('Map does not exist in store')
+        else:
+            del self._maps[map.name]
+
+    def get(self, name):
+        """ Retrieves a map from the store by its name. """
+        if not name in self._maps:
+            return None
+        else:
+            return self._maps[name]
+
+    def list_all(self):
+        """ Lists all the maps in the store. """
+        for _map in self._maps:
+            yield _map
+
+    def save(self):
+        """ Saves the store to the database. """
+        pass    # TODO: we don't have a database yet
